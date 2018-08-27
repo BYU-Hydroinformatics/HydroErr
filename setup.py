05a2e997a2f821532a9da7bcd8c76b97b365a692
waderoberts123 <32141163+waderoberts123@users.noreply.github.com>
2018-08-27 12:26:06 -0600

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+
+setup(
+    name='HydroErr',
+    packages=['HydroErr'],
+    version='1.00',
+    description='Goodness of fit metrics for use in comparison studies, specifically for use in the field '
+                'of hydrology',
+    author='Wade Roberts',
+    author_email='waderoberts123@gmail.com',
+    url='https://github.com/waderoberts123/HydroErr',
+    download_url='https://github.com/waderoberts123/Hydrostats/archive/1.00.tar.gz',
+    keywords=['hydrology', 'error', 'metrics', 'comparison', 'statistics', 'forecast', 'observed'],
+    # arbitrary keywords
+    classifiers=["License :: OSI Approved :: MIT License",
+                 "Programming Language :: Python :: 2.7",
+                 "Programming Language :: Python :: 3.5",
+                 "Programming Language :: Python :: 3.6",
+                 ],
+    install_requires=[
+        'numpy',
+        'numba',
+        'scipy',
+    ],
+    extras_require={
+        'docs': [
+            'sphinx',
+            'sphinxcontrib-napoleon'
+        ]
+    },
+)
