d327dc094aa256f3a52626999c617325130aafd6
wade <waderoberts123@gmail.com>
2019-03-22 12:27:54 -0600

    Updates to documentation for readthedocs.io

--- docs/conf.py
+++ docs/conf.py
@@ -15,8 +15,6 @@
 import os
 import sys
 
-sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
-
 sys.path.insert(0, os.path.abspath('..'))
 sys.path.insert(0, os.path.abspath('.'))
 
