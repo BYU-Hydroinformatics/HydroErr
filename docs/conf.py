f95fd43fde33bd340796d2d2043e4f9b43015f5c
pythonweb2 <wade.a.roberts@protonmail.com>
2025-12-04 14:34:29 -0700

    add matplotlib for docs, remove setup.py, remove coding utf-8 stuff

--- docs/conf.py
+++ docs/conf.py
@@ -1,7 +1,5 @@
-# -*- coding: utf-8 -*-
-#
-# Configuration file for the Sphinx documentation builder.
-#
+"""Configuration file for the Sphinx documentation builder."""
+
 # This file does only contain a selection of the most common options. For a
 # full list see the documentation:
 # http://www.sphinx-doc.org/en/master/config
--- src/HydroErr/__init__.py
+++ src/HydroErr/__init__.py
@@ -1,7 +1,19 @@
-# -*- coding: utf-8 -*-
-"""
-For full documentation, see https://waderoberts123.github.io/HydroErr/
+"""A library of goodness of fit metrics that measure hydrologic skill.
+
+Each metric is contained in a function, and every function has the parameters to treat missing
+values as well as remove zero and negative values from the timeseries data.
+
+Each function contains two properties, name and abbr. These can be used in the Hydrostats package
+when creating tables and adding metrics to the plots. Link to the hydrostats package:
+https://github.com/BYU-Hydroinformatics/Hydrostats.
+
+An example of this functionality is shown below.
+
+>>> import HydroErr as he
+>>> he.acc.name
+'Anomaly Correlation Coefficient'
+>>> he.acc.abbr
+'ACC'
 """
 
-from __future__ import absolute_import
 from .HydroErr import *
